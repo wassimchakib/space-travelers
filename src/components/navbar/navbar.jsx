e3ca26591e356837c18e7f07c0eeeb4f1f127de9
belalgamal46 <belalgamal00@gmail.com>
2022-12-19 15:35:14 +0200

    Create empty navbar and downloaded planet logo

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,10 @@
+import logo from '../../assets/planet.png';
+
+const Navbar = () => (
+  <div>
+    <img src={logo} alt="planet-logo" />
+    <h1>Space Travelers</h1>
+  </div>
+);
+
+export default Navbar;
