45323a0ae1934d91d499ec5ffd55c9439c1702ce
belalgamal46 <belalgamal00@gmail.com>
2022-12-20 18:54:10 +0200

    Display missions and update some styles

--- src/components/missions/index.jsx
+++ src/components/missions/index.jsx
@@ -1,76 +1,52 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getMissions } from '../../redux/missions/missionsSlice';
+
 import './styles.css';
 
-const Missions = () => (
-  <section>
-    <div className="container">
-      <table className="table-container">
-        <thead id="thead">
-          <tr>
-            <th className="th">Missions</th>
-            <th className="th">Description</th>
-            <th className="th">status</th>
-            <th className="th" aria-label="empty-table-head" />
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td className="td mission-title">Thaicom</td>
-            <td className="td">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae esse consectetur
-              inventore quod possimus veniam ipsum ab quam recusandae ad eius minus in nulla
-              veritatis, dolorem doloremque harum sed non, itaque nesciunt? Dolorum dolore
-              asperiores debitis! Exercitationem, optio esse quasi nisi in, obcaecati fuga eum
-              cumque mollitia aliquid culpa officia.
-            </td>
-            <td className="td">
-              <span className="status-not-a-member">NOT A MEMBER</span>
-            </td>
-            <td className="td">
-              <button className="join-mission-btn" type="button">
-                Join Mission
-              </button>
-            </td>
-          </tr>
-          <tr>
-            <td className="td">Thaicom</td>
-            <td className="td">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae esse consectetur
-              inventore quod possimus veniam ipsum ab quam recusandae ad eius minus in nulla
-              veritatis, dolorem doloremque harum sed non, itaque nesciunt? Dolorum dolore
-              asperiores debitis! Exercitationem, optio esse quasi nisi in, obcaecati fuga eum
-              cumque mollitia aliquid culpa officia.
-            </td>
-            <td className="td">
-              <span className="status-not-a-member">NOT A MEMBER</span>
-            </td>
-            <td className="td">
-              <button className="join-mission-btn" type="button">
-                Join Mission
-              </button>
-            </td>
-          </tr>
-          <tr>
-            <td className="td">Thaicom</td>
-            <td className="td">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae esse consectetur
-              inventore quod possimus veniam ipsum ab quam recusandae ad eius minus in nulla
-              veritatis, dolorem doloremque harum sed non, itaque nesciunt? Dolorum dolore
-              asperiores debitis! Exercitationem, optio esse quasi nisi in, obcaecati fuga eum
-              cumque mollitia aliquid culpa officia.
-            </td>
-            <td className="td">
-              <span className="status-not-a-member">NOT A MEMBER</span>
-            </td>
-            <td className="td">
-              <button className="join-mission-btn" type="button">
-                Join Mission
-              </button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  </section>
-);
+const Missions = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getMissions());
+  }, [dispatch]);
+
+  const missions = useSelector((state) => state.missionsReducer.missions);
+  const missionsList = missions.map(
+    ({ description, mission_id: missionId, mission_name: missionName }) => (
+      <tr key={missionId}>
+        <td className="td title-container">
+          <h1 className="mission-title">{missionName}</h1>
+        </td>
+        <td className="td">{description}</td>
+        <td className="td td-with-span">
+          <span className="status-not-a-member">NOT A MEMBER</span>
+        </td>
+        <td className="td button-td">
+          <button className="join-mission-btn" type="button">
+            Join Mission
+          </button>
+        </td>
+      </tr>
+    )
+  );
+  return (
+    <section>
+      <div className="container">
+        <table className="table-container">
+          <thead id="thead">
+            <tr>
+              <th className="th">Missions</th>
+              <th className="th">Description</th>
+              <th className="th">Status</th>
+              <th className="th" aria-label="empty-table-head" />
+            </tr>
+          </thead>
+          <tbody>{missionsList}</tbody>
+        </table>
+      </div>
+    </section>
+  );
+};
 
 export default Missions;
